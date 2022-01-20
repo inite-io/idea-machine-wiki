87a735d87d98d876db467bdc137cd5d036d29e0e
Andrei Ivanouski <aivanouski@gmail.com>
2022-01-20 10:06:09 +0300

    sidebar links update

--- sidebars.js
+++ sidebars.js
@@ -2,16 +2,30 @@ module.exports = {
   someSidebar: {
     "🌀 Inite": ["home"],
     "🗺 Introduction to Inite": [
+      "enter-metaverse/🔭-the-inite-game",
+      "enter-metaverse/how-does-inite-work-1",
+      "enter-metaverse/why-is-inite-unique",
+      "enter-metaverse/cognitive-importance-of-the-inite-platform",
       "enter-metaverse/join-inite",
     ],
     "🤨 Ideanomics": [
-      "articles/inite-game",
+      "articles/idea-as-a-new-type-of-asset",
+      "articles/monetization-of-the-inite-platform",
+      "articles/tokenization-of-ideas",
+      "articles/tokenomics",
     ],
     "🧐 Inite Gaming Mechanics": [
-      "how-does-it-work/summary",
+      "how-does-it-work/daily-tasks-1",
+      "how-does-it-work/creation-of-ideas-2",
+      "how-does-it-work/types-of-ideas-on-the-inite-platform",
+      "how-does-it-work/types-of-idea-challenges-on-the-inite-platform",
+      "how-does-it-work/how-to-come-up-with-an-idea",
+      "how-does-it-work/clan-system",
+      "how-does-it-work/combat-system-and-skills",
     ],
     "More Information about Inite": [
       "more/more",
+      "more/key-audience",
     ],
   },
 };
